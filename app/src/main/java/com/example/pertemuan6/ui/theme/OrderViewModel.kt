901459117c8a68cd009b307364dd2348571134f8
Thoreeq16 <thoriqhidayatullah16@gmail.com>
2023-11-16 00:15:59 +0700

    menambah function setNama pada OrderViewModel.kt

--- app/src/main/java/com/example/pertemuan6/ui/theme/OrderViewModel.kt
+++ app/src/main/java/com/example/pertemuan6/ui/theme/OrderViewModel.kt
@@ -7,12 +7,24 @@ import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 import android.icu.text.NumberFormat
 import androidx.lifecycle.ViewModel
+import com.example.pertemuan6.data.FormData
 
 private const val HARGA_PER_CUP = 3000
 
 class OrderViewModel : ViewModel(){
     private val _stateUI = MutableStateFlow(OrderUIState())
+    private val _nameSTATE = MutableStateFlow(FormData())
     val stateUI: StateFlow<OrderUIState> = _stateUI.asStateFlow()
+    val nameST : StateFlow<FormData> = _nameSTATE.asStateFlow()
+
+
+    fun setNama(list: MutableList<String>){
+        _nameSTATE.update { stateSaatIni -> stateSaatIni.copy(
+            nama = list[0],
+            alamat = list[1],
+            phone = list[2]
+        ) }
+    }
 
     fun setJumlah(jmlEsJumbo: Int) {
         _stateUI.update { stateSaatIni ->
